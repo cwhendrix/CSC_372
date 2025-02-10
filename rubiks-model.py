18d6c1ed4f96be92465e0bf1b67f3d79ac9dd4c6
Cooper Hendrix <107560339+cwhendrix@users.noreply.github.com>
2025-02-10 09:03:18 -0500

    First Setup

---
+++ rubiks-model.py
@@ -0,0 +1,13 @@
+### A1 - Model the 2x2 Cube ###
+# Cooper Hendrix
+# CSC 372 - Dr. Thomas Allen
+# Centre College - 2025
+
+### Setup Cube FCN ###
+# This function sets up the 2x2 cube and returns it
+# The 2x2 cube is modeled with six 2-D arrays 
+# 
+# 
+# 
+# 
+# 
